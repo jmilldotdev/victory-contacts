5e47a71a3b7ec317792ee83e0a49f84f9cd8de99
Jonathan Miller <jon@jmill.dev>
2022-10-17 16:57:22 -0400

    Remappings

--- src/Victory.sol
+++ src/Victory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
 import "@jbx-protocol/juice-contracts-v3/contracts/abstract/JBOperatable.sol";
 import "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBController.sol";
